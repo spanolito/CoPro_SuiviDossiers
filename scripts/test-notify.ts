016a4e81005f8f163919d6c670e640cd7a5ddaf2
spanolito <andujar.oscar@gmail.com>
2026-03-23 01:16:42 +0100

    test: Add script to test admin new user notification.

---
+++ scripts/test-notify.ts
@@ -0,0 +1,9 @@
+import { notifyAdminNewUser } from './src/lib/utils/notifications'
+
+async function runTest() {
+  console.log('Sending test email via trigger...')
+  await notifyAdminNewUser({ nomAffiche: 'Test User Registration', email: 'test@example.com' })
+  console.log('Test finished.')
+}
+
+runTest()
